6bef74638dc0707f5fe235de76eaf49c0f9211e7
michael2904 <michael.abdallah.minciotti@gmail.com>
2015-01-17 08:57:27 -0500

    first

---
+++ myFridge/CategoryViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewCell.swift
+//  myFridge
+//
+//  Created by Michael Abdallah-Minciotti on 2015-01-17.
+//  Copyright (c) 2015 Michael Abdallah-Minciotti. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewCell: UICollectionViewCell {
+
+    @IBOutlet var fruitImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
